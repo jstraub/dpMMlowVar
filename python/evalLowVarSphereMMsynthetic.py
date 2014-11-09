cc37aa53354d7b6ef075064334182bd37be9d00f
Julian Straub <jstraub@csail.mit.edu>
2014-11-08 23:01:12 -0500

    evalLowVarSphereMMsynthetic.py in paper

--- python/evalLowVarSphereMMsynthetic.py
+++ python/evalLowVarSphereMMsynthetic.py
@@ -25,10 +25,13 @@ mpl.rc('lines',linewidth=4.)
 figSize = (12, 14)
 
 #paper
-mpl.rc('font',size=30) 
+mpl.rc('font',size=40) 
 mpl.rc('lines',linewidth=4.)
 figSize = (14, 5.5)
 figSize = (14, 12)
+legendSize = 35
+
+mpl.rc('figure', autolayout=True)
 
 
 def mutualInfo(z,zGt):
@@ -148,7 +151,10 @@ bases = ['DPvMFmeans','spkm']
 cfg['nParms'] = 50;
 paramBase = {'spkm':np.floor(np.linspace(70,10,cfg['nParms'])).astype(int), # 60,2
   'DPvMFmeans':np.array([ang for ang in np.linspace(10.,45.,cfg['nParms'])])}
-paramName =  {'spkm':"K",'DPvMFmeans':"$\lambda$"}
+paramName =  {'spkm':"$K$",'DPvMFmeans':"$\lambda$"}
+baseMap={'spkm':'spkm','kmeans':'k-means','NiwSphere':'DirSNIW', \
+  'DpNiw':'DP-GMM','DpNiwSphere':'DpSNIW opt','DpNiwSphereFull':'DP-TGMM', \
+  'DPvMFmeans':'DP-vMF-means'}
 
 print paramBase
 
@@ -222,10 +228,6 @@ for i,base in enumerate(bases):
 
 print "done with the runs"
 
-baseMap={'spkm':'spkm','kmeans':'k-means','NiwSphere':'DirSNIW', \
-  'DpNiw':'DP-GMM','DpNiwSphere':'DpSNIW opt','DpNiwSphereFull':'DP-TGMM', \
-  'DPvMFmeans':'DPvMFmeans'}
-
 #cl = cm.gnuplot2(np.arange(len(bases)))
 cl = cm.hsv(np.arange(255))
 cl = cm.brg(np.arange(255))
@@ -247,7 +249,7 @@ I = len(bases) +1
 #  plt.legend(loc='lower right')
 #plt.tight_layout()
 
-ipdb.set_trace()
+#ipdb.set_trace()
 
 indSpkm = np.ones(len(paramBase['spkm']),dtype=bool)
 indSpkm[Ns['spkm'].mean(axis=1) < Ns['DPvMFmeans'].min()] = False
@@ -259,49 +261,93 @@ mis['spkm'] = mis['spkm'][indSpkm,:]
 Ns['spkm'] = Ns['spkm'][indSpkm,:]
 Sils['spkm'] = Sils['spkm'][indSpkm,:]
 
-def plotOverParams(values,name):
+def plotOverParams(values,name,showLeg=None):
+  colA = colorScheme('labelMap')['orange']
+  colB = colorScheme('labelMap')['turquoise']
+
   fig = plt.figure(figsize=figSize, dpi=80, facecolor='w', edgecolor='k')
   ax1 = plt.subplot(111)
   base = 'spkm'
   Nmean = Ns[base].mean(axis=1)
   valMean = values[base].mean(axis=1)
   valStd = values[base].std(axis=1)
-  print name,base,valMean
-  print name,base,valStd
-  ax1.plot(valMean,paramBase[base],label=baseMap[base],c=cl[(0+1)*255/I])
-  ax1.plot(valMean-valStd,paramBase[base],'--',label=baseMap[base],c=cl[(0+1)*255/I],lw=2,alpha=0.7)
-  ax1.plot(valMean+valStd,paramBase[base],'--',label=baseMap[base],c=cl[(0+1)*255/I],lw=2,alpha=0.7)
-  ax1.fill_betweenx(paramBase[base],valMean-valStd , valMean+valStd, color=cl[(0+1)*255/I], alpha=0.3)
+#  print name,base,valMean
+#  print name,base,valStd
+  leg1 = ax1.plot(valMean,paramBase[base],label=baseMap[base],c=colA)
+  ax1.plot(valMean-valStd,paramBase[base],'--',label=baseMap[base],c=colA,lw=2,alpha=0.7)
+  ax1.plot(valMean+valStd,paramBase[base],'--',label=baseMap[base],c=colA,lw=2,alpha=0.7)
+  ax1.fill_betweenx(paramBase[base],valMean-valStd , valMean+valStd, color=colA, alpha=0.3)
+  if name == '$K$':
+    leg1 += ax1.plot([30]*len(paramBase[base]), paramBase[base], label="$K_{GT}=30$", c=colorScheme('labelMap')['red'])
   iKtrue = np.where(np.abs(Nmean-30)<2)
-  ax1.plot(values[base].mean(axis=1)[iKtrue],paramBase[base][iKtrue],'x',label=baseMap[base]+' $K={}$'.format(Nmean[iKtrue]),c=(1,0,0))
-  ax1.set_ylabel(paramName[base])  
+  if not name == '$K$':
+    ax1.plot(values[base].mean(axis=1)[iKtrue],paramBase[base][iKtrue],'x',mew=4,ms=15,label=baseMap[base]+' $K={}$'.format(Nmean[iKtrue]),c=colorScheme('labelMap')['red'])
+  ax1.set_ylabel(paramName[base],color=colA)  
   ax1.set_ylim(paramBase[base].min(),paramBase[base].max())
   ax1.invert_yaxis()
+  for  tl in ax1.get_yticklabels():
+    tl.set_color(colA)
+  tiks = ax1.get_yticks()
+  tikLbl = [str(tik) for tik in tiks[:-1]] 
+  tikLbl += [''] 
+  ax1.set_yticklabels(tikLbl)
+#  ax1.legend(loc='best')
   ax1.set_xlabel(name)  
-  ax1.legend(loc='best')
+  if name == 'NMI':
+    ipdb.set_trace()
+    tiks = ax1.get_xticks()
+    tikLbl = []
+    for tik in tiks[::2]:
+      tikLbl += [str(tik),'']
+    del tikLbl[-1]
+    ax1.set_xticklabels(tikLbl)
+
   ax2 = ax1.twinx()
   base = 'DPvMFmeans'
   Nmean = Ns[base].mean(axis=1)
   valMean = values[base].mean(axis=1)
   valStd = values[base].std(axis=1)
-  print name,base,valMean
-  print name,base,valStd
-  ax2.plot(values[base].mean(axis=1),paramBase[base],label=baseMap[base],c=cl[(1+1)*255/I])
-  ax2.plot(valMean-valStd,paramBase[base],'--',label=baseMap[base],c=cl[(1+1)*255/I],lw=2,alpha=0.7)
-  ax2.plot(valMean+valStd,paramBase[base],'--',label=baseMap[base],c=cl[(1+1)*255/I],lw=2,alpha=0.7)
-  ax2.fill_betweenx(paramBase[base],valMean-valStd , valMean+valStd, color=cl[(1+1)*255/I], alpha=0.3)
+#  print name,base,valMean
+#  print name,base,valStd
+  leg2 = ax2.plot(values[base].mean(axis=1),paramBase[base],label=baseMap[base],c=colB)
+  ax2.plot(valMean-valStd,paramBase[base],'--',label=baseMap[base],c=colB,lw=2,alpha=0.7)
+  ax2.plot(valMean+valStd,paramBase[base],'--',label=baseMap[base],c=colB,lw=2,alpha=0.7)
+  ax2.fill_betweenx(paramBase[base],valMean-valStd , valMean+valStd, color=colB, alpha=0.3)
   iKtrue = np.where(np.abs(Nmean-30)<2)
-  ax2.plot(values[base].mean(axis=1)[iKtrue],paramBase[base][iKtrue],'x',label=baseMap[base]+' $K={}$'.format(Nmean[iKtrue]),c=(1,0,0))
-  ax2.set_ylabel(paramName[base])  
+  if not name == '$K$':
+    ax2.plot(values[base].mean(axis=1)[iKtrue],paramBase[base][iKtrue],'x',mew=4,ms=15,label=baseMap[base]+' $K={}$'.format(Nmean[iKtrue]),c=colorScheme('labelMap')['red'])
+  ax2.set_ylabel(paramName[base],color=colB)  
+  for  tl in ax2.get_yticklabels():
+    tl.set_color(colB)
+  tiks = ax2.get_yticks()
+  tikLbl = ax2.get_yticklabels()
+  tikLbl = [''] 
+  tikLbl += [str(tik) for tik in tiks[1:]] 
+  ax2.set_yticklabels(tikLbl)
   ax2.set_ylim(paramBase[base].min(),paramBase[base].max())
-  ax2.legend(loc='best')
+  if name == 'NMI':
+    ipdb.set_trace()
+    tiks = ax2.get_xticks()
+    tikLbl = []
+    for tik in tiks[::2]:
+      tikLbl += [str(tik),'']
+    del tikLbl[-1]
+    ax2.set_xticklabels(tikLbl)
+  if not showLeg is None and showLeg:
+    legs = leg2+leg1
+    labs = [leg.get_label() for leg in legs]
+    ax2.legend(legs,labs,loc='best',prop={'size':legendSize})
   plt.tight_layout()
-  plt.savefig(cfg['outName']+'_{}.png'.format(name),figure=fig)
+  plt.subplots_adjust(right=0.85)
+  plt.savefig(cfg['outName']+'_{}.pdf'.format(re.sub('\$','',name)),figure=fig)
+ 
 
-plotOverParams(mis,'MI')
-plotOverParams(nmis,'NMI')
-plotOverParams(Ns,'numberOfClusters')
-plotOverParams(Sils,'silhouette')
+plotOverParams(mis,'MI',showLeg=False)
+plotOverParams(nmis,'NMI',showLeg=False)
+plotOverParams(Ns,'$K$',showLeg=True)
+plotOverParams(Sils,'silhouette',showLeg=False)
+
+plt.show()
 
 #fig = plt.figure(figsize=figSize, dpi=80, facecolor='w', edgecolor='k')
 #ax1 = plt.subplot(111)
@@ -323,24 +369,23 @@ plotOverParams(Sils,'silhouette')
 #plt.tight_layout()
 #plt.savefig(cfg['outName']+'_Sil.png',figure=fig)
 
-fig = plt.figure(figsize=figSize, dpi=80, facecolor='w', edgecolor='k')
-ax1 = plt.subplot(111)
-base = 'spkm'
-ax1.plot(Ns[base].mean(axis=1),paramBase[base],label=baseMap[base],c=cl[(0+1)*255/I])
-ax1.plot([30]*len(paramBase[base]), paramBase[base], label="true number of clusters $K=30$", c=(1,0,0))
-ax1.set_ylabel(paramName[base])  
-ax1.invert_yaxis()
-ax1.set_xlabel("number of clusters")  
-ax1.legend(loc='best')
-ax2 = ax1.twinx()
-base = 'DPvMFmeans'
-ax2.plot(Ns[base].mean(axis=1),paramBase[base],label=baseMap[base],c=cl[(1+1)*255/I])
-ax2.set_ylabel(paramName[base])  
-ax2.legend(loc='right')
-plt.tight_layout()
-plt.savefig(cfg['outName']+'_nClusters.png',figure=fig)
+#fig = plt.figure(figsize=figSize, dpi=80, facecolor='w', edgecolor='k')
+#ax1 = plt.subplot(111)
+#base = 'spkm'
+#ax1.plot(Ns[base].mean(axis=1),paramBase[base],label=baseMap[base],c=cl[(0+1)*255/I])
+#ax1.plot([30]*len(paramBase[base]), paramBase[base], label="true number of clusters $K=30$", c=(1,0,0))
+#ax1.set_ylabel(paramName[base])  
+#ax1.invert_yaxis()
+#ax1.set_xlabel("number of clusters")  
+#ax1.legend(loc='best')
+#ax2 = ax1.twinx()
+#base = 'DPvMFmeans'
+#ax2.plot(Ns[base].mean(axis=1),paramBase[base],label=baseMap[base],c=cl[(1+1)*255/I])
+#ax2.set_ylabel(paramName[base])  
+#ax2.legend(loc='right')
+#plt.tight_layout()
+#plt.savefig(cfg['outName']+'_nClusters.png',figure=fig)
 
-plt.show()
 
 fig = plt.figure(figsize=figSize, dpi=80, facecolor='w', edgecolor='k')
 for i,base in enumerate(bases):

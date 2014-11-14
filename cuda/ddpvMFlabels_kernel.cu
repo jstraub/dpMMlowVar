eab88768790676b7eb07d5d11f8028565fb57a86
Julian Straub <jstraub@csail.mit.edu>
2014-11-14 00:02:44 -0500

    small angle approximation for labels on gpu

--- cuda/ddpvMFlabels_kernel.cu
+++ cuda/ddpvMFlabels_kernel.cu
@@ -32,7 +32,43 @@ __device__ inline T distToUninstantiated( T zeta, T age, T beta, T w, T Q, T thr
 
     dPhi = f/df;
     phi = phi - dPhi; // Newton iteration
-    printf("i=%d: prev: dPhi=%; phi=%f; curr: dPhi=%f phi=%f zeta=%f; w=%f; Q=%f; f=%f; df=%f; sinPhi=%f; beta=%f %f %f \n",i,dPhiPrev,phiPrev,dPhi,phi,zeta,w,Q,f,df,sinPhi,beta,a,b);
+    printf("i=%d: prev: dPhi=%f; phi=%f; curr: dPhi=%f phi=%f zeta=%f; w=%f; Q=%f; f=%f; df=%f; sinPhi=%f; beta=%f %f %f \n",i,dPhiPrev,phiPrev,dPhi,phi,zeta,w,Q,f,df,sinPhi,beta,a,b);
+//    printf("i=%d: dPhi=%f zeta=%f; age=%f; beta=%f; w=%f; Q=%f; thresh=%f; \n",i,dPhi,zeta,age,beta,w,Q,thresh);
+    if(fabs(dPhi) < thresh) break;
+  }
+
+  T theta = asin(beta/w *sin(phi));
+  T eta = asin(beta*sin(phi));
+
+  return w*(cos(theta)-1.0) + age*(Q+beta*(cos(phi)-1.)) + cos(eta);
+}
+
+template<typename T, uint32_t I>
+__device__ inline T distToUninstantiatedSmallAngleApprox( T zeta, T age, T beta, T w, T Q, T thresh)
+{
+  // solveProblem2(x_i, zeta, this->ts_[k], this->ws_[k], phi,theta,eta);
+  // solves
+  // (1)  sin(phi) beta = sin(theta)
+  // (2)  zeta = T phi + theta
+  // for phi and theta
+
+  T phi =  0.0;
+  T dPhi = 0.0;
+  for (uint32_t i=0; i< I; ++i)
+  {
+    T a = (beta*phi);
+    T b = (beta/w *phi);
+    T f = -zeta + (beta*phi) + (age * phi) + (beta/w *phi);
+    T df = beta + age + (beta/w);
+//    T df = age + (beta*cosPhi)/sqrt(1.-beta*beta*sinPhi*sinPhi) 
+//      + (beta*cosPhi)/sqrt(w*w - beta*beta*sinPhi*sinPhi); 
+
+    T phiPrev = phi;
+    T dPhiPrev = dPhi;
+
+    dPhi = f/df;
+    phi = phi - dPhi; // Newton iteration
+    printf("i=%d: prev: dPhi=%f; phi=%f; curr: dPhi=%f phi=%f zeta=%f; w=%f; Q=%f; f=%f; df=%f; beta=%f %f %f \n",i,dPhiPrev,phiPrev,dPhi,phi,zeta,w,Q,f,df,beta,a,b);
 //    printf("i=%d: dPhi=%f zeta=%f; age=%f; beta=%f; w=%f; Q=%f; thresh=%f; \n",i,dPhi,zeta,age,beta,w,Q,thresh);
     if(fabs(dPhi) < thresh) break;
   }
@@ -91,7 +127,9 @@ __global__ void ddpvMFlabelAssign_kernel(T *d_q, T *d_p, uint32_t *z,
         if(Ns[k] == 0)
         {// cluster not instantiated yet in this timestep                           
           T age = d_ages[k]; // TODO d_ages size is not always = K
-          sim_k = distToUninstantiated<T,10>(zeta,age,beta,d_ws[k],Q,1e-6);
+          //TODO: using small angle approximation here!
+          sim_k = distToUninstantiatedSmallAngleApprox<T,10>(zeta,age,beta,d_ws[k],Q,1e-6);
+//          sim_k = distToUninstantiated<T,10>(zeta,age,beta,d_ws[k],Q,1e-6);
         }else{ // cluster instantiated                                              
           sim_k = dot;
         }

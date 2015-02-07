4acce39bd60221cd4bf5b4ca207ad2bea4c53151
Julian Straub <jstraub@csail.mit.edu>
2015-02-07 16:59:29 -0500

    dumpStats

--- include/clusterer.hpp
+++ include/clusterer.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <fstream>
 #include <global.hpp>
 #include <clData.hpp>
 
@@ -61,7 +62,7 @@ public:
 
   virtual T silhouette();
 
-//  virtual void dumpStats(std::ofstream& fout);
+  virtual void dumpStats(std::ofstream& fout);
 
 protected:
   uint32_t K_;
@@ -160,13 +161,13 @@ T Clusterer<T,DS>::silhouette()
 //  return sil.sum()/static_cast<T>(N_);
 };
 
-//template<class T, class DS>
-//void Clusterer<T,DS>::dumpStats(std::ofstream& fout)
-//{
-//  fout<<this->K_<<" "<<this->cost_<<" ";
-//  for(uint32_t k=0; k< this->K_; ++k)
-//    fout<<this->cls_[k]->N()<<" ";
-//  for(uint32_t k=0; k< this->K_-1; ++k)
-//    fout<<this->cls_[k]->globalId<<" ";
-//  fout<<this->cls_[this->K_-1]->globalId<<endl;
-//};
+template<class T, class DS>
+void Clusterer<T,DS>::dumpStats(std::ofstream& fout)
+{
+  fout<<this->K_<<" "<<this->cost_<<" ";
+  for(uint32_t k=0; k< this->K_; ++k)
+    fout<<this->cls_[k]->N()<<" ";
+  for(uint32_t k=0; k< this->K_-1; ++k)
+    fout<<(this->cls_[k]->globalId)<<" ";
+  fout<<(this->cls_[this->K_-1]->globalId)<<endl;
+};
